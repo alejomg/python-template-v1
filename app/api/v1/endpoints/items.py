9f8dcc770d62701ac67a5cd3c5226315c031353a
alejo <mens.leonis@gmail.com>
2026-04-27 19:51:13 -0700

    separated schema

--- app/api/v1/endpoints/items.py
+++ app/api/v1/endpoints/items.py
@@ -1,17 +1,11 @@
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel, Field
+from app.schemas.item import Item
 
 router = APIRouter()
 
-# Data schema defined with Pydantic
-class ItemCreate(BaseModel):
-    name: str = Field(..., min_length=3, example="Laptop Gamer")
-    price: float = Field(..., gt=0)
-    stock: int = Field(default=10, ge=0)
-
 
 @router.post("/", status_code=201)
-async def create_item(item: ItemCreate):
+async def create_item(item: Item):
     # Here Pydantic already validated item properties
     if item.price > 10000:
         raise HTTPException(status_code=400, detail="Price too high")
